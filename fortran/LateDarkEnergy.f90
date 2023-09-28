9876258719eeac9ce7de0172553b06a02744b64f
Diogo Souza <diogohenriquefrsz@gmail.com>
2023-09-27 20:55:51 -0400

    Binned quadratic w(z) implemented for two bins

--- fortran/LateDarkEnergy.f90
+++ fortran/LateDarkEnergy.f90
@@ -32,6 +32,9 @@ module LateDE
         class(TLateDE) :: this
         real(dl), intent(in) :: a    
         real(dl) :: w_de, z
+        real(dl) :: w0,w1,w2,z0,z1,z2
+        real(dl) :: wa0,waa0,wa1,waa1
+        real(dl) :: Delta_z1, Delta_w1, Delta_z2, Delta_w2
         
         w_de = 0
 
@@ -96,15 +99,42 @@ module LateDE
                 w_de = this%w10  
             end if
         else if (this%model == 6) then
-            ! 2 binned linear w. Linearity is in redshift, not in scale factor
+            ! 2 binned linear w(z). Linearity is in redshift, not in scale factor
             z = 1._dl/a - 1._dl
             if (z < this%z1) then
                 w_de = this%w0 + (this%w1 - this%w0)/this%z1 * z
             else if (z < this%z2) then
                 w_de = this%w1 + (this%w2 - this%w1)/(this%z2 - this%z1) * (z - this%z1)
             else
-                w_de = this%w3
-            end if    
+                w_de = this%w2
+            end if  
+        else if (this%model == 7) then
+            ! 2 binned quadratic w(z)
+            ! Definitions
+            z0=0
+            z1=this%z1
+            z2=this%z2 
+            w0=this%w0 
+            w1=this%w1 
+            w2=this%w2
+            Delta_z1 = z1-z0
+            Delta_w1 = w1-w0
+            Delta_z2 = z2-z1
+            Delta_w2 = w2-w1
+            ! Boundary conditions -see my notes-
+            wa0  = 2._dl*(Delta_w1/Delta_z1 - Delta_w2/Delta_z2)
+            waa0 = -Delta_w1/Delta_z1**2 + 2._dl*Delta_w2/(Delta_z1*Delta_z2)
+            wa1  =  2._dl*Delta_w2/Delta_z2
+            waa1 =  -Delta_w2/Delta_z2**2
+            ! Equation of state
+            z = 1._dl/a - 1._dl
+            if (z < this%z1) then
+                w_de = w0 + wa0 * (z-z0) + waa0*(z-z0)**2
+            else if (z < this%z2) then
+                w_de = w1 + wa1 * (z-z1) + waa1*(z-z1)**2
+            else
+                w_de = w2
+            end if                
         else
             stop "[Late Fluid DE] Invalid Dark Energy Model"   
         end if
@@ -114,8 +144,14 @@ module LateDE
         class(TLateDE) :: this
         real(dl), intent(in) :: a
         real(dl) :: grho_de, z    
-        real(dl) :: wa0,wa1,wa2
+        ! real(dl) :: wa0,wa1,wa2
         real(dl) :: alpha0,alpha1,alpha2
+
+        real(dl) :: w0,w1,w2,z0,z1,z2
+        real(dl) :: wa0,waa0,wa1,waa1,wa2,waa2
+        real(dl) :: Delta_z1, Delta_w1, Delta_z2, Delta_w2  
+        real(dl) :: A00,A10,A20, A01,A11,A21, A02,A12,A22      
+
 
         ! Returns 8*pi*G * rho_de, no factor of a^4
         grho_de = 0
@@ -181,7 +217,7 @@ module LateDE
                 grho_de = grho_de_today * this%fac10 * a**(-3 * (1 + this%w10))
             end if    
         else if (this%model == 6) then
-            ! 3 Bins CPL 
+            ! 2 Bins linear w(z)
             z = 1._dl/a - 1
 
             wa0 = (this%w1-this%w0)/(this%z1-0)
@@ -198,7 +234,50 @@ module LateDE
                 grho_de = grho_de_today*(1+this%z1)**alpha0*exp(3*wa0*this%z1)*((1+z)/(1+this%z1))**alpha1*exp(3*wa1*(z-this%z1))
             else
                 grho_de = grho_de_today*(1+this%z1)**alpha0*exp(3*wa0*this%z1)*((1+this%z2)/(1+this%z1))**alpha1*exp(3*wa1*(this%z2-this%z1))*((1+z)/(1+this%z2))**alpha2*exp(3*wa2*(z-this%z2))
-            end if                
+            end if 
+        else if(this%model == 7) then 
+            ! 2 bins quadratic w(z)
+            z0=0
+            z1=this%z1
+            z2=this%z2 
+            w0=this%w0 
+            w1=this%w1 
+            w2=this%w2
+            Delta_z1 = z1-z0
+            Delta_w1 = w1-w0
+            Delta_z2 = z2-z1
+            Delta_w2 = w2-w1   
+
+            wa0  = 2._dl*(Delta_w1/Delta_z1 - Delta_w2/Delta_z2)
+            waa0 = -Delta_w1/Delta_z1**2 + 2._dl*Delta_w2/(Delta_z1*Delta_z2)
+            wa1  =  2._dl*Delta_w2/Delta_z2
+            waa1 =  -Delta_w2/Delta_z2**2 
+            wa2  = 0
+            waa2 = 0                     
+
+            A00 = 3*(1+w0-wa0*z0+waa0*z0**2)
+            A00 = 3*(wa0-2*waa0*z0)
+            A00 = 3*waa0
+
+            A01 = 3*(1+w1-wa1*z1+waa1*z1**2)
+            A01 = 3*(wa1-2*waa1*z1)
+            A01 = 3*waa1
+
+            A02 = 3*(1+w2-wa2*z2+waa2*z2**2)
+            A02 = 3*(wa2-2*waa2*z2)
+            A02 = 3*waa2
+
+            z = 1._dl/a - 1
+            if (z < this%z1) then
+                grho_de = grho_de_today*((1+z)/(1+z0))**(A00-A10+A20)*exp((A10-A20)*(z-z0)+A20*(z**2-z0**0)/2)
+            else if (z < this%z2) then
+                grho_de = grho_de_today*((1+z1)/(1+z0))**(A00-A10+A20)*exp((A10-A20)*(z1-z0)+A20*(z1**2-z0**0)/2) * &
+                          ((1+z)/(1+z1))**(A01-A11+A21)*exp((A11-A21)*(z-z1)+A21*(z**2-z1**0)/2)
+            else
+                grho_de = grho_de_today*((1+z1)/(1+z0))**(A00-A10+A20)*exp((A10-A20)*(z1-z0)+A20*(z1**2-z0**0)/2) * &
+                          ((1+z2)/(1+z1))**(A01-A11+A21)*exp((A11-A21)*(z2-z1)+A21*(z2**2-z1**0)/2) * &
+                          ((1+z)/(1+z2))**(A02-A12+A22)*exp((A12-A22)*(z-z2)+A22*(z**2-z2**0)/2)          
+            end if            
         else 
             stop "[Late Fluid DE] Invalid Dark Energy Model"
         end if
